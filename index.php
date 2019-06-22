168f083dfd6b029d58dfbe3c0f436c0843ae2381
Darren Tarrant <in@djtarrant.co.uk>
2019-06-22 23:22:39 +0100

    Add scrolling months

--- index.php
+++ index.php
@@ -20,14 +20,37 @@ require_once("calendar.class.php");
         <![endif]-->
         <?php 
         $djt_CalendarObject = new djt_Calendar();
+        if( isset($_GET['startMonth']) && preg_match("/^\d+$/", $_GET['startMonth']) ){
+            $djt_CalendarObject->startMonth = $_GET['startMonth'];
+        }else{
+            $djt_CalendarObject->startMonth = date("n");
+        }
+        if( isset($_GET['startYear']) && preg_match("/^\d+$/", $_GET['startYear']) ){
+            $djt_CalendarObject->startYear = $_GET['startYear'];
+        }else{
+            $djt_CalendarObject->startYear = date("Y");
+        }
+        if( ($djt_CalendarObject->startMonth-$djt_CalendarObject->numMonths)<=0){
+            $previousMonth = ($djt_CalendarObject->startMonth-$djt_CalendarObject->numMonths)+12;
+        }else{
+            $previousMonth = $djt_CalendarObject->startMonth-$djt_CalendarObject->numMonths;
+        }
+        if( ($djt_CalendarObject->startMonth+$djt_CalendarObject->numMonths)>12){
+            $nextMonth = ($djt_CalendarObject->startMonth+$djt_CalendarObject->numMonths)-12;
+        }else{
+            $nextMonth = $djt_CalendarObject->startMonth+$djt_CalendarObject->numMonths;
+        }
+        $djt_CalendarObject->showCalendarLink($previousMonth, $direction = 'backwards');
         $djt_CalendarObject->showCalendar();
+        $djt_CalendarObject->showCalendarLink($nextMonth, $direction = 'forward');
         
-        //TODO - scroll calendar < and > for more months
+        //TODO - mobile CSS styling
+        //TODO - apply PWA
+        //TODO - data storage
         //TODO - click on a calendar day to see the TODO for the day = all day, general and specific time, ordering
         //TODO - on a calendar day - CRUD TODO - time = all day, general and specific time, ordering (push up/down order), recurring (day, week, month, year)
         // => categories: appointment, todolist, birthday, moon, friends and family, work
-        //TODO - calendar page, visual indication of todo on the day - colorised and abbreviated based on category
-        //TODO - mobile styling        
+        //TODO - calendar page, visual indication of todo on the day - colorised and abbreviated based on category        
         //TODO - PWA caching - how to cache a CRUD action?
         //TODO - auth of some kind, and perhaps logging
         ?>
