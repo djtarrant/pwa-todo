e86574e5875be7dcf500bcf60a709cf990e2fc76
Darren Tarrant <in@djtarrant.co.uk>
2019-06-29 21:31:10 +0100

    PWA images

--- index.php
+++ index.php
@@ -7,10 +7,30 @@ require_once("calendar.class.php");
 
     <head>
         <meta charset="utf-8">
-        <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <title></title>
-        <meta name="description" content="">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>TODO</title>
+        <meta name="description" content="TODO">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <script src="js/main.js"></script>
+        <link rel="apple-touch-icon" sizes="76x76" href="apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png">
+        <link rel="manifest" href="manifest.json">
+        <link rel="mask-icon" href="safari-pinned-tab.svg" color="#5bbad5">
+        <meta name="msapplication-TileColor" content="#da532c">
+        <meta name="theme-color" content="#752424">
+        <meta name="apple-mobile-web-app-capable" content="yes">
+        <meta name="apple-mobile-web-app-status-bar-style" content="default">
+        <link rel="apple-touch-startup-image" href="/apple-touch-icon.png">
+        <!-- iOS Splash Screen Images -->
+        <link rel="apple-touch-startup-image" href="apple-splash-640.png" media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)">
+        <link rel="apple-touch-startup-image" href="apple-splash-750.png" media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)">
+        <link rel="apple-touch-startup-image" href="apple-splash-1242.png" media="(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)">
+        <link rel="apple-touch-startup-image" href="apple-splash-1125.png" media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)">
+        <link rel="apple-touch-startup-image" href="apple-splash-1536.png" media="(min-device-width: 768px) and (max-device-width: 1024px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)">
+        <link rel="apple-touch-startup-image" href="apple-splash-1668.png" media="(min-device-width: 834px) and (max-device-width: 834px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)">
+        <link rel="apple-touch-startup-image" href="apple-splash-2048.png" media="(min-device-width: 1024px) and (max-device-width: 1024px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)">
+
         <link rel="stylesheet" href="css/main.css">
     </head>
 
@@ -18,6 +38,12 @@ require_once("calendar.class.php");
         <!--[if lte IE 9]>
             <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
         <![endif]-->
+        <section id="addToHomeScreen" class = "alertBox">
+            <strong>Install App</strong><br/>Add our app to your home screen?<br/>
+            <a href="javascript:void(0)" onClick="hidePrompt()">No,&nbsp;Thanks</a> <button id = "addToHomeScreen" onClick="installApp()">Yes, Please!</button> 
+        </section>
+
+
         <?php 
         $djt_CalendarObject = new djt_Calendar();
         if( isset($_GET['startMonth']) && preg_match("/^\d+$/", $_GET['startMonth']) ){
